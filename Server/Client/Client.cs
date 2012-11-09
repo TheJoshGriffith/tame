26d765d5932a4a358ea1a6d3a0393fe17cb0a741
XtrmJosh@gmail.com <XtrmJosh@gmail.com@a6fe9a78-abcb-f5f6-4c92-1dc1b4bc9d17>
2012-11-09 23:18:40 +0000

    Added new version, now working as a server but needs a multithreading issue resolving in the server, the client sends messages, parser will be added soon.

---
+++ Server/Client/Client.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public partial class Client : Form
+    {
+        ASCIIEncoding encoding = new ASCIIEncoding();
+        IPEndPoint ipEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2000);
+        Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+            conBut.Enabled = true;
+            button3.Enabled = false;
+        }
+
+        private void conBut_Click(object sender, EventArgs e)
+        {
+            sock.Connect(ipEnd);
+            byte[] buffer = new byte[32];
+            sock.Receive(buffer);
+            listBox1.Items.Add(encoding.GetString(buffer));
+            conBut.Enabled = false;
+            button3.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] mabyte = encoding.GetBytes(textBox1.Text);
+            sock.Send(mabyte);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sock.Disconnect(true);
+            conBut.Enabled = true;
+            button3.Enabled = false;
+        }
+    }
+}
---
+++ Server/Server/Server.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Tame
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.conButton = new System.Windows.Forms.Button();
+            this.timerListen = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 201);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(240, 186);
+            this.listBox1.TabIndex = 4;
+            // 
+            // conButton
+            // 
+            this.conButton.Location = new System.Drawing.Point(15, 227);
+            this.conButton.Name = "conButton";
+            this.conButton.Size = new System.Drawing.Size(75, 23);
+            this.conButton.TabIndex = 5;
+            this.conButton.Text = "Connect";
+            this.conButton.UseVisualStyleBackColor = true;
+            this.conButton.Click += new System.EventHandler(this.conButton_Click);
+            // 
+            // timerListen
+            // 
+            this.timerListen.Tick += new System.EventHandler(this.timerListen_Tick);
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 262);
+            this.Controls.Add(this.conButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Server";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Server_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button conButton;
+        private System.Windows.Forms.Timer timerListen;
+    }
+}
+
